371d3d45c01037208164141155410923f52193b3
Gustavo Marques <gugamello2014@yahoo.com>
2022-09-08 12:58:41 -0300

    chore: fix test watch script, add ts-jest dependency

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    '.+\\.ts$': 'ts-jest',
+  },
+};
